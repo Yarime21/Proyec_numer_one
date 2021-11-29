0388cc1f6e631653d4c5979c55d37fda6f799c2e
sala 305 <sala305@poliacademico.edu.loc>
2021-11-29 07:51:16 -0500

    Proyectouno

---
+++ index.js
@@ -0,0 +1,10 @@
+//const chalk =require ("chalk")
+//console.log(chalk.blue('Hello world!'));
+
+const express =require ("express")
+const app = express()
+app.listen (3000,()=> {
+
+    console.log ("servidor corriendo")
+
+})
