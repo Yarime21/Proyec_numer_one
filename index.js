42a02fab694f41c61958764f1525ab2c5e6a2fa7
sala 305 <sala305@poliacademico.edu.loc>
2021-12-01 07:58:15 -0500

    actualizacion dos

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-//const chalk =require ("chalk")
-//console.log(chalk.blue('Hello world!'));
+// const chalk =require ("chalk")
+// console.log(chalk.blue('Hello world!'));
 
-const express =require ("express")
+const express = require('express')
 const app = express()
-app.listen (3000,()=> {
-
-    console.log ("servidor corriendo")
-
+app.get ('/home', (req,res) => { 
+    res.send('<h1> Hola Mundo </h1>')
+})
+app.get ('/page', (req,res) => { 
+    res.send( {message: "Hola Mundo"} )
+})
+app.listen(3001, () => {
+  console.log('servidor corriendo')
 })
